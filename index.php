2ae750a301d1c245c0d829ef3d12089107d343c1
J. Oppenlaender <j.oppenlaender@web.de>
2016-01-31 19:27:54 +0000

    route logic

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require_once __DIR__.'/vendor/autoload.php';
 
 use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
 
 class MyApp extends Application {
     use Application\TwigTrait;
@@ -18,12 +19,11 @@ class SpfResponse { # extends stdClass
     private $response;
     public function __construct () {
         # parent::__construct();
-        $r = array(
+        $this->response = (object) array(
             'head' => '<!-- Styles -->',
             'body' => new stdClass(),
             'foot' => '<!-- Scripts -->',
         );
-        $this->response = (object) $r;
     }
     public function setBody(array $arr) {
         $this->response->body = $arr;
@@ -33,6 +33,15 @@ class SpfResponse { # extends stdClass
     }
 }
 
+$checkSpfRoute = function (Request $request, MyApp $app) {
+    if ($request->query->get('spf') === 'navigate') {
+        // must return json-wrapped html fragments
+        $app->isSpfRequest = true;
+    } else {
+        // must return the full html page
+        $app->isSpfRequest = false;
+    }
+};
 
 
 
@@ -46,47 +55,36 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 # INDEX
 $app->get('/', function () use ($app) {
     return $app['twig']->render('base.twig', array(
-    	'header' => $app['twig']->render('header.twig', array()),
+    	'header'  => $app['twig']->render('header.twig', array()),
     	'content' => $app['twig']->render('index.twig', array()),
     ));
 });
 
-# HEADER (loaded with ajax)
-// $app->get('/header', function () use($app) {
-//     $spf = new SpfResponse();
-//     $spf->setBody(['header' => $app['twig']->render('header.twig', array())]);
-//     return json_encode($spf->getResponse());
-// });
-// $app->get('/footer', function () use($app) {
-//     $spf = new SpfResponse();
-//     $spf->setBody(['footer' => $app['twig']->render('footer.twig', array())]);
-//     return json_encode($spf->getResponse());
-// });
-
-# NAVBAR (loaded with ajax)
-$app->get('/navbar', function () use($app) {
-    $spf = new SpfResponse();
-    $spf->setBody(['navbar' => $app['twig']->render('navbar.twig', array())]);
-    return json_encode($spf->getResponse());
-});
-
-// $app->get('/index', function () use($app) {
-//     $spf = new SpfResponse();
-//     $spf->setBody(['content' => $app['twig']->render('index.twig', array())]);
-//     return json_encode($spf->getResponse());
-// });
-
 $app->get('/photos', function () use($app) {
-    $spf = new SpfResponse();
-    $spf->setBody(['content' => $app['twig']->render('photos.twig', array())]);
-    return json_encode($spf->getResponse());
-});
+    if ($app->isSpfRequest === true) {
+        $spf = new SpfResponse();
+        $spf->setBody(['content' => $app['twig']->render('photos.twig', array())]);
+        return json_encode($spf->getResponse());
+    } else {
+        return $app['twig']->render('base.twig', array(
+            'header'  => $app['twig']->render('header.twig', array()),
+            'content' => $app['twig']->render('photos.twig', array()),
+        ));
+    }
+})->before($checkSpfRoute);
 
-$app->get('/videos', function () use($app) {
-    $spf = new SpfResponse();
-    $spf->setBody(['content' => $app['twig']->render('videos.twig', array())]);
-    return json_encode($spf->getResponse());
-});
+$app->get('/videos', function (Request $request) use($app) {
+    if ($app->isSpfRequest === true) {
+        $spf = new SpfResponse();
+        $spf->setBody(['content' => $app['twig']->render('videos.twig', array())]);
+        return json_encode($spf->getResponse());
+    } else {
+        return $app['twig']->render('base.twig', array(
+            'header'  => $app['twig']->render('header.twig', array()),
+            'content' => $app['twig']->render('videos.twig', array()),
+        ));
+    }
+})->before($checkSpfRoute);
 
 $app['debug'] = true;
 
