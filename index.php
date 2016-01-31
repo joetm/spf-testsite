120ca1c73e37e286276847fdaef1f49abd91eadf
J. Oppenlaender <j.oppenlaender@web.de>
2016-01-31 19:08:59 +0000

    twig base template

--- index.php
+++ index.php
@@ -14,6 +14,28 @@ class MyApp extends Application {
     # use Application\TranslationTrait;
 }
 
+class SpfResponse { # extends stdClass
+    private $response;
+    public function __construct () {
+        # parent::__construct();
+        $r = array(
+            'head' => '<!-- Styles -->',
+            'body' => new stdClass(),
+            'foot' => '<!-- Scripts -->',
+        );
+        $this->response = (object) $r;
+    }
+    public function setBody(array $arr) {
+        $this->response->body = $arr;
+    }
+    public function getResponse() {
+	return $this->response;
+    }
+}
+
+
+
+
 $app = new MyApp();
 
 // set up twig
@@ -23,57 +45,47 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 # INDEX
 $app->get('/', function () use ($app) {
-    return $app['twig']->render('index.twig', array());
+    return $app['twig']->render('base.twig', array(
+    	'header' => $app['twig']->render('header.twig', array()),
+    	'content' => $app['twig']->render('index.twig', array()),
+    ));
 });
 
 # HEADER (loaded with ajax)
-$app->get('/header', function () use($app) {
-    $response = (object) array(
-        'head' => '<!-- Styles -->',
-        'body' => new stdClass(),
-        'foot' => '<!-- Scripts -->',
-    );
-    $response->body->header = $app['twig']->render('header.twig', array());
-    return json_encode($response);
-});
+// $app->get('/header', function () use($app) {
+//     $spf = new SpfResponse();
+//     $spf->setBody(['header' => $app['twig']->render('header.twig', array())]);
+//     return json_encode($spf->getResponse());
+// });
+// $app->get('/footer', function () use($app) {
+//     $spf = new SpfResponse();
+//     $spf->setBody(['footer' => $app['twig']->render('footer.twig', array())]);
+//     return json_encode($spf->getResponse());
+// });
+
 # NAVBAR (loaded with ajax)
 $app->get('/navbar', function () use($app) {
-    $response = (object) array(
-        'head' => '<!-- Styles -->',
-        'body' => new stdClass(),
-        'foot' => '<!-- Scripts -->',
-    );
-    $response->body->navbar = $app['twig']->render('navbar.twig', array());
-    return json_encode($response);
-});
-$app->get('/footer', function () use($app) {
-    $response = (object) array(
-        'head' => '<!-- Styles -->',
-        'body' => new stdClass(),
-        'foot' => '<!-- Scripts -->',
-    );
-    $response->body->footer = $app['twig']->render('footer.twig', array());
-    return json_encode($response);
+    $spf = new SpfResponse();
+    $spf->setBody(['navbar' => $app['twig']->render('navbar.twig', array())]);
+    return json_encode($spf->getResponse());
 });
 
+// $app->get('/index', function () use($app) {
+//     $spf = new SpfResponse();
+//     $spf->setBody(['content' => $app['twig']->render('index.twig', array())]);
+//     return json_encode($spf->getResponse());
+// });
+
 $app->get('/photos', function () use($app) {
-    $response = (object) array(
-        'head' => '<!-- Styles -->',
-        'body' => new stdClass(),
-        'foot' => '<!-- Scripts -->',
-    );
-    $response->body->content = $app['twig']->render('photos.twig', array());
-    return json_encode($response);
+    $spf = new SpfResponse();
+    $spf->setBody(['content' => $app['twig']->render('photos.twig', array())]);
+    return json_encode($spf->getResponse());
 });
 
 $app->get('/videos', function () use($app) {
-    $response = (object) array(
-        'head' => '<!-- Styles -->',
-        'body' => new stdClass(),
-        'foot' => '<!-- Scripts -->',
-    );
-    $response->body->content = $app['twig']->render('videos.twig', array());
-    return json_encode($response);
+    $spf = new SpfResponse();
+    $spf->setBody(['content' => $app['twig']->render('videos.twig', array())]);
+    return json_encode($spf->getResponse());
 });
 
 $app['debug'] = true;
